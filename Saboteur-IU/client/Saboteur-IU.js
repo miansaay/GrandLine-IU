814b1e3df2180c6ef80f5b733785c1a5bbac69db
scarro <sergioca22@gmail.com>
2015-11-23 23:04:47 +0100

    actualizacion

--- Saboteur-IU/client/Saboteur-IU.js
+++ Saboteur-IU/client/Saboteur-IU.js
@@ -1,30 +1,38 @@
 // Posiciones = new Meteor.Collection('cartas');
 
-Router.configure({
-  layoutTemplate: 'main'
-});
+	var cartas = new Object(); 
+	var context = new Object();
+
+	Router.configure({
+  		layoutTemplate: 'main'
+	});
 
 
-if (Meteor.isClient) {  
-  Template.play.onRendered(function(){
-      var canvas = document.getElementById('canvas1');
-      var ctx = canvas.getContext('2d');
+	if (Meteor.isClient) {  
+		
+	  	Template.play.onRendered(function(){
 
-      var newGame = new Game();
-      newGame.init(ctx);
-  });
+		  var canvas = document.getElementById('canvas1');
+		  cartas = document.getElementById('cartas');
+		  context = canvas.getContext('2d');
 
-  Template.play.events({
-    'mousedown #canvas1': function(event){
-      var x = event.offsetX;
-      var y = event.offsetY;
-      console.log("has pulsado: [" + x + "," + y + "]");
-    },
+	      var newGame = new Game();
+	      newGame.init(context);
+	  });
 
-    'mouseup #canvas1': function(event){
-      var x = event.offsetX;
-      var y = event.offsetY;
-      console.log("has soltado en: [" + x + "," + y + "]");
-    }
-  });
-}
+	  Template.play.events({
+	    'mousedown #canvas1': function(event){
+	      var x = event.offsetX;
+	      var y = event.offsetY;
+	      console.log("has pulsado: [" + x + "," + y + "]");
+	      imagenCartas.draw(context, cartas, "camino1", x, y);
+
+	    },
+
+	    'mouseup #canvas1': function(event){
+	      var x = event.offsetX;
+	      var y = event.offsetY;
+	      console.log("has soltado en: [" + x + "," + y + "]");
+	    }
+	  });
+	}
